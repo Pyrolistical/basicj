f2f86960cefcad536899085710eef531a78429b4
Pyrolistical <pyrogx1133@gmail.com>
2005-02-14 07:06:59 +0000

    forgot to add gpl block

--- test/basicj/ScreenTest.java
+++ test/basicj/ScreenTest.java
@@ -1,6 +1,20 @@
-/*
- * Created on Jan 27, 2005 by Ronald Chen
- */
+/*----------------------------------------------------------------------------
+ This file is part of BasicJ.
+
+ BasicJ is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ BasicJ is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with BasicJ; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ----------------------------------------------------------------------------*/
 package basicj;
 
 import java.awt.*;
